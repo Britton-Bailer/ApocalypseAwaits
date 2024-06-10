be72d1f775a23d5678262310aec8507192c7638b
Britton Bailer <britton.bailer@gmail.com>
2024-06-10 12:28:10 -0400

    Initial Commit
    +player movement
    +tilesets and test map
    +simple zombie pathing

---
+++ scripts/player.gd
@@ -0,0 +1,27 @@
+extends CharacterBody2D
+
+
+const SPEED = 150.0
+
+func _physics_process(delta):
+
+	# set velocity to 0 when not moving
+	velocity = Vector2(0, 0)
+
+	var left = Input.is_action_pressed("left")
+	if left:
+		velocity.x = -SPEED
+		
+	var right = Input.is_action_pressed("right")
+	if right:
+		velocity.x = SPEED
+	
+	var up = Input.is_action_pressed("up")
+	if up:
+		velocity.y = -SPEED
+		
+	var down = Input.is_action_pressed("down")
+	if down:
+		velocity.y = SPEED
+	
+	move_and_slide()
