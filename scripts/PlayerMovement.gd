899c8d9d3ab8362db4aea0924b10b761eb426e93
Britton Bailer <britton.bailer@gmail.com>
2024-06-14 12:30:22 -0400

    Update zombie movement
    +include raycasts to keep zombie away from obstacles locally

--- scripts/PlayerMovement.gd
+++ scripts/PlayerMovement.gd
@@ -1,26 +1,22 @@
-extends CharacterBody2D
+extends RigidBody2D
 
 const SPEED = 150.0
 
 func _physics_process(delta):
 
-	# set velocity to 0 when not moving
-	velocity = Vector2(0, 0)
-
 	var left = Input.is_action_pressed("left")
 	if left:
-		velocity.x = -SPEED
+		linear_velocity.x = -SPEED
 		
 	var right = Input.is_action_pressed("right")
 	if right:
-		velocity.x = SPEED
+		linear_velocity.x = SPEED
 	
 	var up = Input.is_action_pressed("up")
 	if up:
-		velocity.y = -SPEED
+		linear_velocity.y = -SPEED
 		
 	var down = Input.is_action_pressed("down")
 	if down:
-		velocity.y = SPEED
-	
-	move_and_slide()
+		linear_velocity.y = SPEED
+
--- scripts/ZombieNavigation.gd
+++ scripts/ZombieNavigation.gd
@@ -2,8 +2,8 @@ extends CharacterBody2D
 
 @export var target: Node2D
 
-var speed = randf_range(80, 120)
-var acceleration = 7
+var speed = randf_range(150, 200)
+var acceleration = 3
 var sightRange = 600
 var lastSeenTarget
 var broadcastRange = 500
@@ -14,23 +14,43 @@ var targetingInterval = 10
 var targetingTimer = 0
 
 @onready var navAgent = $NavigationAgent2D
-@onready var lineOfSightRay = $ShapeCast2D
-@onready var player = %player
+@onready var lineOfSightRay = $RayCastToPlayer
+
+
 var zombiesContainer # parent that holds all zombies
+var directions = []
+var dirWeights = []
+var numDirections = 12
+var senseDistance = 50
+@onready var navigation_raycasts = %NavigationRaycasts
 
 ## runs one time before anything else
 func _ready():
 	target = player
 	zombiesContainer = get_parent()
 	lastSeenTarget = position
+	
+	for i in range(0, 360, (360/numDirections)):
+		var dir = Vector2(cos(deg_to_rad(i)), sin(deg_to_rad(i))).normalized()
+		var newRaycast = RayCast2D.new()
+		navigation_raycasts.add_child(newRaycast)
+		newRaycast.set_collision_mask_value(1, true)
+		newRaycast.set_collision_mask_value(2, true)
+		newRaycast.target_position = dir * senseDistance
+		
+		#start with all being normalized (length = 1)
+		directions.push_back(dir)
+		dirWeights.push_back(0)
 
 ## runs every frame
-func _physics_process(delta):	
+func _physics_process(delta):
 	do_targeting()
 	
 	#do navigation and movement
+	run_directions_calculations()
 	navigation(delta)
 	move_and_slide()
+	#queue_redraw()
 
 func do_targeting():
 	if(targetingTimer > targetingInterval):
@@ -61,17 +81,24 @@ func do_targeting():
 	targetingTimer += 1
 
 func navigation(delta):
-	var direction = Vector2.ZERO
+	var direction = directions[0]
 	
-	direction = navAgent.get_next_path_position() - global_position
-	direction = direction.normalized()
+	var desiredDirection = navAgent.get_next_path_position() - global_position
+	
+	#move along directions[i] that is longes
+	var maxDirWeight = 0
+	for i in range(directions.size()):
+		if(dirWeights[i] > dirWeights[maxDirWeight]):
+			maxDirWeight = i
+	
+	direction = directions[maxDirWeight].normalized()
 	
 	velocity = velocity.lerp(direction * speed, acceleration * delta)
 
-## when other zombie broadcasts, this is used to update last seen position with slight random offset
+## when other zombie broadcasts, this is used to update last seen position
 func set_target(newPosition):
 	if(!can_see_target()):
-		lastSeenTarget = newPosition + Vector2(randf_range(-50, 50), randf_range(-50, 50))
+		lastSeenTarget = newPosition
 	
 func broadcast_position(newPosition):
 	for zombie in zombiesContainer.get_children():
@@ -80,7 +107,7 @@ func broadcast_position(newPosition):
 			zombie.set_target(newPosition)
 
 func can_see_target():
-	return transform.origin.distance_to(target.transform.origin) < sightRange && lineOfSightRay.get_collision_count() == 0
+	return transform.origin.distance_to(target.transform.origin) < sightRange && !lineOfSightRay.is_colliding()
 
 ## zombie either cannot reach point or has already reached it
 func needs_new_point():
@@ -94,3 +121,27 @@ func take_damage(amt):
 	#if health is at or below 0, delete zombie
 	if(health <= 0):
 		queue_free()
+
+func run_directions_calculations():
+	var desiredDirection = navAgent.get_next_path_position() - global_position
+	for i in range(directions.size()):
+		dirWeights[i] = desiredDirection.normalized().dot(directions[i])
+		
+		if(navigation_raycasts.get_child(i).is_colliding()):
+			dirWeights[i] -= 5
+			if(i+1 < directions.size()):
+				dirWeights[i+1] -= 1
+			else:
+				dirWeights[0] -= 1
+				
+			if(i > 0):
+				dirWeights[i-1] -= 1
+			else:
+				dirWeights[dirWeights.size()-1] -= 1
+
+func _draw():
+	for i in range(directions.size()):
+		if(dirWeights[i] > 0):
+			draw_line(Vector2.ZERO, (directions[i] * dirWeights[i] * senseDistance), Color.BLACK, 2)
+		else:
+			draw_line(Vector2.ZERO, (directions[i] * -clampf(dirWeights[i], -1, 0) * senseDistance), Color.RED, 2)
