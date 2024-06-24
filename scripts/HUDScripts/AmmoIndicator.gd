c7611406a0b79880d74161922095ac49e54fe161
Britton Bailer <britton.bailer@gmail.com>
2024-06-24 16:05:38 -0400

    Hud update
    ammo indicator circle bigger

--- scripts/HUDScripts/AmmoIndicator.gd
+++ scripts/HUDScripts/AmmoIndicator.gd
@@ -4,7 +4,7 @@ var endAngle = 360
 var weapon
 
 func _draw():
-	draw_arc(Vector2.ZERO, 20, deg_to_rad(-90), deg_to_rad(-90 + endAngle), 20, Color.BLACK, 2)
+	draw_arc(Vector2.ZERO, 50, deg_to_rad(-90), deg_to_rad(-90 + endAngle), 50, Color.BLACK, 10)
 
 func _process(_delta):
 	if (weapon == null):
