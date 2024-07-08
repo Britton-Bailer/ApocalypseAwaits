d1fd0b3e7f8a657cb47373062c1ad9dd96358d6a
Britton Bailer <britton.bailer@gmail.com>
2024-07-08 15:15:09 -0400

    Fix base stats for health/stamina
    +added 2 new items

--- scripts/HUDScripts/HealthIndicator.gd
+++ scripts/HUDScripts/HealthIndicator.gd
@@ -12,7 +12,7 @@ func _draw():
 	draw_rect(Rect2((-width/2) + 20, 0, width, height), Color.BLACK, false, 1)
 	
 	#red health bar inside (current health)
-	draw_rect(Rect2((-width/2) + 20, 0.5, (width / player.maxHealth) * player.health, height-1), Color.DARK_RED, true)
+	draw_rect(Rect2((-width/2) + 20, 0.5, (width / player.maxHealth) * player.health, height - 1), Color.DARK_RED, true)
 
 func _process(_delta):
 	queue_redraw()
--- scripts/HUDScripts/StaminaIndicator.gd
+++ scripts/HUDScripts/StaminaIndicator.gd
@@ -1,18 +1,18 @@
 extends Control
 
 var player
-#width of bar
 var width = 600.0
 var height = 10
 
 func _draw():
 	if (player == null):
 		return
-	#bar outline (max health)
+		
+	#bar outline (max stamina)
 	draw_rect(Rect2((-width/2) + 20, 0, width, height), Color.BLACK, false, 1)
 	
-	#red health bar inside (current health)
-	draw_rect(Rect2((-width/2) + 20, 0.5, (width/player.maxStamina) * player.stamina, height-1), Color.SKY_BLUE, true)
+	#blue stamina bar inside (current stamina)
+	draw_rect(Rect2((-width/2) + 20, 0.5, (width / player.maxStamina) * player.stamina, height - 1), Color.SKY_BLUE, true)
 
 func _process(_delta):
 	queue_redraw()
