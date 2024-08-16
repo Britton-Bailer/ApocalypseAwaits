a317d283b347e3a11243aad7a1a2547b3f2064b9
Britton Bailer <britton.bailer@gmail.com>
2024-08-16 12:28:18 -0400

    Mouse sensitivity

--- scripts/CustomClasses/GunWeapon.gd
+++ scripts/CustomClasses/GunWeapon.gd
@@ -22,19 +22,19 @@ var reloadTimer = 0
 var mag
 var player
 
-#func _draw():
-	#draw_circle_arc_poly(Vector2.ZERO, bullet.range, 89-bullet.spread, 91+bullet.spread, Color(1,1,1,0.2))
-#
-#func draw_circle_arc_poly(center, radius, angle_from, angle_to, color):
-	#var nb_points = 32
-	#var points_arc = PackedVector2Array()
-	#points_arc.push_back(center)
-	#var colors = PackedColorArray([color])
-#
-	#for i in range(nb_points + 1):
-		#var angle_point = deg_to_rad(angle_from + i * (angle_to - angle_from) / nb_points - 90)
-		#points_arc.push_back(center + Vector2(cos(angle_point), sin(angle_point)) * radius)
-	#draw_polygon(points_arc, colors)
+func _draw():
+	draw_circle_arc_poly(Vector2.ZERO, bullet.range, 89-bullet.spread, 91+bullet.spread, Color(1,1,1,0.2))
+
+func draw_circle_arc_poly(center, radius, angle_from, angle_to, color):
+	var nb_points = 32
+	var points_arc = PackedVector2Array()
+	points_arc.push_back(center)
+	var colors = PackedColorArray([color])
+
+	for i in range(nb_points + 1):
+		var angle_point = deg_to_rad(angle_from + i * (angle_to - angle_from) / nb_points - 90)
+		points_arc.push_back(center + Vector2(cos(angle_point), sin(angle_point)) * radius)
+	draw_polygon(points_arc, colors)
 
 func __ready():
 	player = get_parent().get_parent()
